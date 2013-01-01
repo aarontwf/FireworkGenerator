4fc81cf86d42812d15ea118f3bce7c0dd506e930
LimeByte <limebyte@twf.co.nz>
2013-01-01 16:09:56 +1300

    Testing loading from an HTML file.

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,4 +1,5 @@
 var effects = new Array();
+var effectform;
 
 function Effect(type, twinkle, trail, colour, fadeColour) {
 	this.type = type;
@@ -9,7 +10,7 @@ function Effect(type, twinkle, trail, colour, fadeColour) {
 }
 
 function addEffect() {
-	effects.push(new Effect('Ball', false, false, 'Silver', 'None'));
+	effects.push(new Effect('Burst', true, true, 'Green', 'Blue'));
 	renderEffects();
 }
 
@@ -28,70 +29,14 @@ function renderEffects() {
 				'            <i class="icon-remove-sign"></i>' +
 				'        </button>' +
 				'    </h4>' +
-				'    <form id="options">' +
-				'        <fieldset id="column1">' +
-				'            <label for="type">Type</label>' +
-				'            <select name="type">' +
-				'                <option>Ball</option>' +
-				'                <option>Ball Large</option>' +
-				'                <option>Star</option>' +
-				'                <option>Burst</option>' +
-				'                <option>Creeper</option>' +
-				'            </select>' +
-				'            <label for="twinkle" class="larger">' +
-				'                Twinkle' +
-				'                <input type="checkbox" name="twinkle" class="pull-right"></input>' +
-				'            </label>' +
-				'            <label for="trail" class="larger">' +
-				'                Trail' +
-				'            <input type="checkbox" name="trail" class="pull-right"></input>' +
-				'            </label>' +
-				'        </fieldset>' +
-				'        <fieldset id="column2">' +
-				'            <label for="colour">Colour</label>' +
-				'            <select name="colour">' +
-				'                <option>White</option>' +
-				'                <option>Silver</option>' +
-				'                <option>Grey</option>' +
-				'                <option>Black</option>' +
-				'                <option>Red</option>' +
-				'                <option>Maroon</option>' +
-				'                <option>Yellow</option>' +
-				'                <option>Olive</option>' +
-				'                <option>Lime</option>' +
-				'                <option>Green</option>' +
-				'                <option>Aqua</option>' +
-				'                <option>Teal</option>' +
-				'                <option>Blue</option>' +
-				'                <option>Navy</option>' +
-				'                <option>Fuchsia</option>' +
-				'                <option>Purple</option>' +
-				'                <option>Orange</option>' +
-				'            </select>' +
-				'            <label for="fade-colour">Fade Colour</label>' +
-				'            <select name="fade-colour">' +
-				'                <option>None</option>' +
-				'                <option>White</option>' +
-				'                <option>Silver</option>' +
-				'                <option>Grey</option>' +
-				'                <option>Black</option>' +
-				'                <option>Red</option>' +
-				'                <option>Maroon</option>' +
-				'                <option>Yellow</option>' +
-				'                <option>Olive</option>' +
-				'                <option>Lime</option>' +
-				'                <option>Green</option>' +
-				'                <option>Aqua</option>' +
-				'                <option>Teal</option>' +
-				'                <option>Blue</option>' +
-				'                <option>Navy</option>' +
-				'                <option>Fuchsia</option>' +
-				'                <option>Purple</option>' +
-				'                <option>Orange</option>' +
-				'            </select>' +
-				'        </fieldset>' +
-				'    </form>' +
+				effectform +
 				'</section>'
 		);
+		
+		$('#effect' + i + ' #type').val(effects[i].type);
+		$('#effect' + i + ' #twinkle').prop('checked', effects[i].twinkle);
+		$('#effect' + i + ' #trail').prop('checked', effects[i].trail);
+		$('#effect' + i + ' #colour').val(effects[i].colour);
+		$('#effect' + i + ' #fade-colour').val(effects[i].fadeColour);
 	}
 }
